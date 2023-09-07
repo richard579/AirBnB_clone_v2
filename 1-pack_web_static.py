a89852eccaa78886197d28ac327a43b0d1082ac9
richard579 <richardbeng358@outlook.com>
2023-09-07 00:09:15 -0700

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,7 +9,7 @@ def do_pack():
     try:
         local("mkdir -p versions")
         local("tar -cvzf versions/web_static_{}.tgz web_static/".
-                format(time.strftime("%Y%m%d%H%M%S")))
+              format(time.strftime("%Y%m%d%H%M%S")))
         return ("versions/web_static_{}.tgz".format(time.
                                                     strftime("%Y%m%d%H%M%S")))
     except:
