d97c075099dc43046e16832d37b5f7a180a3c0b0
richard579 <richardbeng358@outlook.com>
2023-09-23 01:03:25 -0700

    5-number_template.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -34,4 +34,4 @@ def python_is_cool(text='is cool'):
 
 if __name__ == '__main__':
     app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000
+    app.run(host='0.0.0.0', port=5000)
--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -47,4 +47,4 @@ def number_template(n):
 
 if __name__ == '__main__':
     app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000
+    app.run(host='0.0.0.0', port=5000)
